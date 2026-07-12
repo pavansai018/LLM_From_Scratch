e0f79017b98c4885ff45dc28670d0eb5f1c82b67
pavansai018 <18pavansai@gmail.com>
2026-07-13 01:58:10 +0800

    added detoxification/config

---
+++ detoxification/config.py
@@ -0,0 +1,22 @@
+GPT2_SMALL_124M: dict = {
+    'emb_dim': 768,
+    'vocab_size': 50257,
+    'num_heads': 12,
+    'n_layers': 12,
+    'context_length': 1024,
+    'drop_rate': 0.1,
+    'qkv_bias': True,
+    'stride': 256,
+}
+
+
+GPT2_MEDIUM_355M: dict = {
+    'emb_dim': 768,
+    'vocab_size': 50257,
+    'num_heads': 12,
+    'n_layers': 12,
+    'context_length': 1024,
+    'drop_rate': 0.1,
+    'qkv_bias': True,
+    'stride': 256,
+}
